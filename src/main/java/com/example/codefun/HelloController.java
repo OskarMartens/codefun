923c21d1b6c68717c2ab273e49324b1b70e9f0ef
OskarMartens <martens.oskar@gmail.com>
2023-11-14 20:30:54 +0100

    good morning version

--- src/main/java/com/example/codefun/HelloController.java
+++ src/main/java/com/example/codefun/HelloController.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/api/")
 public class HelloController {
 
-    @GetMapping()
+    @GetMapping("hello")
     public ResponseEntity<String> helloWorld(){
-        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+    }
+
+    @GetMapping("morning")
+    public ResponseEntity<String> goodMorning(){
+        return new ResponseEntity<>("Good Morning World", HttpStatus.OK);
     }
 }

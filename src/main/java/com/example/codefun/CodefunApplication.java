699ba575187e4c675099f74530587edd6291d6ee
OskarMartens <martens.oskar@gmail.com>
2023-11-14 13:59:45 +0100

    first commit

---
+++ src/main/java/com/example/codefun/CodefunApplication.java
@@ -0,0 +1,14 @@
+package com.example.codefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodefunApplication.class,
+                              args);
+    }
+
+}
---
+++ src/test/java/com/example/codefun/CodefunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codefun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
